c07bc35ff18a533c5e0bec4180e78f07392f5d4f
Vlado Tonković <vltonkov@vsite.hr>
2024-11-07 17:40:47 +0100

    Added MVC application

---
+++ WebApplication-PIN1/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WebApplication_PIN1.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
